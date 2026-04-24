2e8e504e3c2d455a6364b9a4c1051fd6793de27c
NOTREGRETO <notregreto@gmail.com>
2026-04-24 22:33:20 +0000

    fix: update nodemailer transporter with service gmail for better reliability

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -17,6 +17,7 @@ export default async function handler(req, res) {
     }
 
     const transporter = nodemailer.createTransport({
+        service: 'gmail',
         host: 'smtp.gmail.com',
         port: 465,
         secure: true,
